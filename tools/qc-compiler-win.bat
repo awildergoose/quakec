64ae19792756858123e491223f1b6eb4e437477a
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-08-21 19:44:13 +0300

    first

--- tools/qc-compiler-win.bat
+++ tools/qc-compiler-win.bat
@@ -4,7 +4,7 @@ CD ../
 
 REM ****** generate hash table ******
 echo Generating Hash Table..
-python3 bin\qc_hash_generator.py -i tools\asset_conversion_table.csv -o source\server\hash_table.qc
+python bin\qc_hash_generator.py -i tools\asset_conversion_table.csv -o source\server\hash_table.qc
 
 REM ****** create build directories ******
 MKDIR build\fte\ 2>nul
@@ -14,13 +14,23 @@ CD bin/
 
 REM ****** build.. ******
 echo Compiling FTE CSQC..
-fteqcc-cli-win.exe -DFTE -Wall -srcfile ../progs/csqc.src
+fteqcc-cli-win.exe -DFTE -Wall -srcfile ../progs/csqc.src | grep -i error
 echo Compiling FTE SSQC..
-fteqcc-cli-win.exe -O3 -DFTE -Wall -srcfile ../progs/ssqc.src
+fteqcc-cli-win.exe -O3 -DFTE -Wall -srcfile ../progs/ssqc.src | grep -i error
 echo Compiling FTE MenuQC..
-fteqcc-cli-win.exe -O3 -DFTE -Wall -srcfile ../progs/menu.src
+fteqcc-cli-win.exe -O3 -DFTE -Wall -srcfile ../progs/menu.src | grep -i error
 echo Compiling Standard/Id SSQC..
-fteqcc-cli-win.exe -O3 -Wall -srcfile ../progs/ssqc.src
+fteqcc-cli-win.exe -O3 -Wall -srcfile ../progs/ssqc.src | grep -i error
 
-echo End of script.
-pause
+SET GAMEDIR=G:\Games\nzp
+
+echo %GAMEDIR%
+
+copy ..\build\fte\csprogs.dat %GAMEDIR%\nzp\csprogs.dat
+copy ..\build\fte\csprogs.lno %GAMEDIR%\nzp\csprogs.lno
+copy ..\build\fte\menu.dat %GAMEDIR%\nzp\menu.dat
+copy ..\build\fte\qwprogs.dat %GAMEDIR%\nzp\qwprogs.dat
+
+pushd "G:\Games\nzp"
+start "" "G:\Games\nzp\nzportable-sdl64.exe"
+popd
