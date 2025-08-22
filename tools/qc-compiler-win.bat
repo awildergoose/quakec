097ab58079ade27c6b2b85a1e8c55cdd290c8120
awildergoose <61917452+awildergoose@users.noreply.github.com>
2025-08-22 15:36:25 +0300

    chore: actually fix networking for the third time

--- tools/qc-compiler-win.bat
+++ tools/qc-compiler-win.bat
@@ -14,23 +14,36 @@ CD bin/
 
 REM ****** build.. ******
 echo Compiling FTE CSQC..
-fteqcc-cli-win.exe -DFTE -Wall -srcfile ../progs/csqc.src | grep -i error
+fteqcc-cli-win.exe -DFTE -Wall -srcfile ../progs/csqc.src > csqc.log
+set ERR=%ERRORLEVEL%
+type csqc.log | grep -e warn -e err -e fatal
+if not %ERR%==0 exit /b %ERR%
+
 echo Compiling FTE SSQC..
-fteqcc-cli-win.exe -O3 -DFTE -Wall -srcfile ../progs/ssqc.src | grep -i error
+fteqcc-cli-win.exe -O3 -DFTE -Wall -srcfile ../progs/ssqc.src > ssqc.log
+set ERR=%ERRORLEVEL%
+type ssqc.log | grep -e warn -e err -e fatal
+if not %ERR%==0 exit /b %ERR%
+
 echo Compiling FTE MenuQC..
-fteqcc-cli-win.exe -O3 -DFTE -Wall -srcfile ../progs/menu.src | grep -i error
+fteqcc-cli-win.exe -O3 -DFTE -Wall -srcfile ../progs/menu.src > menu.log
+set ERR=%ERRORLEVEL%
+type menu.log | grep -e warn -e err -e fatal
+if not %ERR%==0 exit /b %ERR%
+
 echo Compiling Standard/Id SSQC..
-fteqcc-cli-win.exe -O3 -Wall -srcfile ../progs/ssqc.src | grep -i error
+fteqcc-cli-win.exe -O3 -Wall -srcfile ../progs/ssqc.src > std.log
+set ERR=%ERRORLEVEL%
+type std.log | grep -e warn -e err -e fatal
+if not %ERR%==0 exit /b %ERR%
 
 SET GAMEDIR=G:\Games\nzp
-
-echo %GAMEDIR%
 
 copy ..\build\fte\csprogs.dat %GAMEDIR%\nzp\csprogs.dat
 copy ..\build\fte\csprogs.lno %GAMEDIR%\nzp\csprogs.lno
 copy ..\build\fte\menu.dat %GAMEDIR%\nzp\menu.dat
 copy ..\build\fte\qwprogs.dat %GAMEDIR%\nzp\qwprogs.dat
 
-pushd "G:\Games\nzp"
-start "" "G:\Games\nzp\nzportable-sdl64.exe"
+pushd "%GAMEDIR%"
+start "" "%GAMEDIR%\nzportable-sdl64.exe" -nosecure -noDTLS -unencrypted
 popd
